001801379814ed6e31eb15df36bd1718f08d15be
Bayani Carbone <bayani.carbone@canonical.com>
2021-07-09 21:03:21 +0000

    added docstrings and removed TODOs

--- README.md
+++ README.md
@@ -1,13 +1,49 @@
-# charm-rancher-integrator
+# Rancher Integrator
 
 ## Description
 
-TODO: Describe your charm in a few paragraphs of Markdown
+This charm deploys and manages the rancher-integration application capable of registering (and if
+needed unregistering) the Kubernetes cluster hosting the charm within an existing Rancher container
+ management platform.
 
-## Usage
+Charmhub page: https://charmhub.io/rancher-integrator  
+Documentation: https://charmhub.io/rancher-integrator/docs  
+Bugs / Issues: https://github.com/baycarbone/charm-rancher-integrator/issues
 
-TODO: Provide high-level usage, such as required config or relations
+More information regarding the rancher-interator application can be found at [rancher-integrator repository](https://github.com/baycarbone/rancher-integrator).
 
+## Quickstart
+
+Assuming you have juju installed and bootstrapped on a Kubernetes cluster, deploy the charm and
+execute the required actions as needed (if you do not, see the next section).
+
+In order to execute the actions the following config options need to be set:
+- url: url of the existing Rancher platform against which registration will be performed.
+
+- bearer-token: bearer-token needed to access the Rancher api. For more information on retrieving
+such a token, please see [rancher-api-keys](https://rancher.com/docs/rancher/v2.5/en/user-settings/api-keys/).
+
+- (optional) cluster-name: useful if you want a user defined cluster name within the Rancher platform. If this
+option is not set, a randomly generated cluster name will be assigned.
+
+- (optional) cert-verify: disables verification of the Rancher platform certificate. This is not
+recommended but can be useful for development purposes when using a self-signed certificate.
+
+```bash
+# Deploy the rancher-integrator charm
+$ juju deploy rancher-integrator \
+  --config url="cluster.test" \
+  --config bearer-token="token-abcde:abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzab"
+
+# Elavate permisions in order for the charm to communicate with the Kubernetes API
+$ juju trust rancher-integrator --scope=cluster
+
+# Wait for the deployment to complete
+$ watch -n1 --color juju status --color
+
+# Execute the registration action
+$ juju run-action rancher-integrator/0 register
+```
 
 ## Developing
 
--- src/charm.py
+++ src/charm.py
@@ -34,111 +34,64 @@ class RancherIntegratorCharm(CharmBase):
         self.framework.observe(self.on.rancher_integrator_pebble_ready, self._on_rancher_integrator_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.register_action, self._on_register_action)
-        self.framework.observe(self.on.delete_action, self._on_delete_action)
+        self.framework.observe(self.on.unregister_action, self._on_unregister_action)
         self.framework.observe(self.on.cluster_control_relation_changed, self._on_cluster_control_relation_changed)
         self._stored.set_default(url=self.config["url"], bearer_token=self.config["bearer-token"], cert_verify=self.config["cert-verify"], manifest=None)
         self.kubernetes = Kubernetes('cattle-system')
+        self.cmd = "sleep 30s"
 
     def _on_rancher_integrator_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API.
+        """Define and start the rancher-integrator workload using the Pebble API.
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        You'll need to specify the right entrypoint and environment
-        configuration for your specific workload. Tip: you can see the
-        standard entrypoint of an existing container using docker inspect
+        As the rancher-integrator is essentially a one and done cli tool,
+        we start the container with a simple sleep command.
 
-        Learn more about Pebble layers at https://github.com/canonical/pebble
         """
 
         # Get a reference the container attribute on the PebbleReadyEvent
         container = event.workload
-        # Define an initial Pebble layer configuration
 
-        pebble_layer = {
-            "summary": "rancher integrator layer",
-            "description": "pebble config layer for rancher integrator",
-            "services": {
-                "rancher-integrator": {
-                    "override": "replace",
-                    "summary": "rancher integrator",
-                    "command": "sleep 30s",
-                    "startup": "enabled",
-                    "environment": {
-                        "TOKEN": self.model.name,
-                    },
-                }
-            },
-        }
         # Add intial Pebble config layer using the Pebble API
-        container.add_layer("rancher-integrator", pebble_layer, combine=True)
+        container.add_layer("rancher-integrator", self._rancher_integrator_layer(), combine=True)
         # Autostart any services that were defined with startup: enabled
         container.autostart()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
         self.unit.status = ActiveStatus()
 
     def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration.
+        """Check if the Rancher API is reachable with new config.
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle config, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the config.py file.
+        If the API connection details are changed,
+        make sure the API is still reachable.
 
-        Learn more about config at https://juju.is/docs/sdk/config
         """
-        # current = self.config["thing"]
-        # if current not in self._stored.things:
-        #     logger.debug("found a new thing: %r", current)
-        #     self._stored.things.append(current)
+        # Implement api reachability check
         pass
 
-
     def _on_cluster_control_relation_changed(self, event):
-        """Just an example to show how to receive actions.
+        """Retrieve API connection details when relating to a charmed Rancher deployment
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
+        If this charm is related to a charmed Rancher platform through
+        cross model relation, retrieve the API connection details from
+        the relation and save them in the stored state.
 
-        Learn more about actions at https://juju.is/docs/sdk/actions
         """
+        # Implement relation handling
         pass
 
     def _on_register_action(self, event):
-        """Just an example to show how to receive actions.
+        """Register the Kubernetes cluster in a Rancher platform.
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
+        Using the configured API connection details, register
+        the Kubernetes cluster into the specified Rancher platform.
 
-        Learn more about actions at https://juju.is/docs/sdk/actions
         """
 
         container = self.unit.get_container("rancher-integrator")
 
-        cmd = "python ./rancher-integrator.py -i -d " + self.config["url"] + " " + self.config["bearer-token"].split(":")[0] + " " + self.config["bearer-token"].split(":")[1] + " register -n" + self.config["cluster-name"]
-
-        pebble_layer = {
-            "summary": "rancher integrator layer",
-            "description": "pebble config layer for rancher integrator",
-            "services": {
-                "rancher-integrator": {
-                    "override": "replace",
-                    "summary": "rancher integrator",
-                    "command": cmd,
-                    "startup": "enabled",
-                    "environment": {
-                        "TOKEN": self.model.name,
-                    },
-                }
-            },
-        }
+        self.cmd = "python ./rancher-integrator.py -i -d " + self.config["url"] + " " + self.config["bearer-token"].split(":")[0] + " " + self.config["bearer-token"].split(":")[1] + " register -n" + self.config["cluster-name"]
 
         # Add intial Pebble config layer using the Pebble API
-        container.add_layer("rancher-integrator", pebble_layer, combine=True)
+        container.add_layer("rancher-integrator", self._rancher_integrator_layer(), combine=True)
         logging.info("Added updated layer 'rancher-integrator' to Pebble plan to run register command")
 
         # Stop the service if it is already running
@@ -158,39 +111,20 @@ class RancherIntegratorCharm(CharmBase):
         # All is well, set an ActiveStatus
         self.unit.status = ActiveStatus()
 
-    def _on_delete_action(self, event):
-        """Just an example to show how to receive actions.
+    def _on_unregister_action(self, event):
+        """Register the Kubernetes cluster in a Rancher platform.
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
+        Using the configured API connection details, register
+        the Kubernetes cluster into the specified Rancher platform.
 
-        Learn more about actions at https://juju.is/docs/sdk/actions
         """
         container = self.unit.get_container("rancher-integrator")
 
-        cmd = "python ./rancher-integrator.py -i -d " + self.config["url"] + " " + self.config["bearer-token"].split(":")[0] + " " + self.config["bearer-token"].split(":")[1] + " delete " + self.config["cluster-name"]
-
-        pebble_layer = {
-            "summary": "rancher integrator layer",
-            "description": "pebble config layer for rancher integrator",
-            "services": {
-                "rancher-integrator": {
-                    "override": "replace",
-                    "summary": "rancher integrator",
-                    "command": cmd,
-                    "startup": "enabled",
-                    "environment": {
-                        "TOKEN": self.model.name,
-                    },
-                }
-            },
-        }
+        self.cmd = "python ./rancher-integrator.py -i -d " + self.config["url"] + " " + self.config["bearer-token"].split(":")[0] + " " + self.config["bearer-token"].split(":")[1] + " unregister " + self.config["cluster-name"]
 
         # Add intial Pebble config layer using the Pebble API
-        container.add_layer("rancher-integrator", pebble_layer, combine=True)
-        logging.info("Added updated layer 'rancher-integrator' to Pebble plan to run register command")
+        container.add_layer("rancher-integrator", self._rancher_integrator_layer(), combine=True)
+        logging.info("Added updated layer 'rancher-integrator' to Pebble plan to run unregister command")
 
         # Stop the service if it is already running
         if container.get_service("rancher-integrator").is_running():
@@ -204,6 +138,24 @@ class RancherIntegratorCharm(CharmBase):
             self.kubernetes.delete(self._stored.manifest)
         # All is well, set an ActiveStatus
         self.unit.status = ActiveStatus()
+
+    def _rancher_integrator_layer(self):
+        """Returns a Pebble configration layer for rancher-integrator"""
+        return {
+            "summary": "rancher-integrator layer",
+            "description": "pebble config layer for rancher-integrator",
+            "services": {
+                "rancher-integrator": {
+                    "override": "replace",
+                    "summary": "rancher-integrator",
+                    "command": self.cmd,
+                    "startup": "enabled",
+                    "environment": {
+                        "TOKEN": self.model.name,
+                    },
+                }
+            },
+        }
 
 if __name__ == "__main__":
     main(RancherIntegratorCharm)
